3a515c75e28040a4b2d55bbace4d5e341a315dff
Aleks Ionin <starik.ionytch@gmail.com>
2022-12-24 19:16:18 +0200

    task 3 solved

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,51 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const form=document.querySelector('.form');
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(event) {
+  event.preventDefault();
+  let delay = Number(event.currentTarget.delay.value);
+  const step = Number(event.currentTarget.step.value);
+  const amount = Number(event.currentTarget.amount.value);
+
+  console.log(delay);
+  console.log(step);
+  console.log(amount);
+  
+  
+  for (let position = 0; position < amount; position++) {
+    createPromise(position, delay)      
+      .then(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.success(`Fulfilled promise ${position} in ${delay}ms`);          
+        }, delay);
+    
+      })
+      .catch(({ position, delay }) => {
+        setTimeout(() => {
+          Notify.failure(`Rejected promise ${position} in ${delay}ms`);
+        }, delay);
+    
+      });
+    delay += step;
   }
 }
+
+  
+
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const objectPromise = { position, delay };
+
+  return new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve(objectPromise);
+    }
+    reject(objectPromise);
+  });
+}
+
+
+
